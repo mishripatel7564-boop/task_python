7fad5251b824415143f42089276d55c7d7bceb98
mishri_patel <mishripatel7564@gmail.com>
2026-01-06 08:25:40 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,37 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=20
+print(x)
+20
+print(type(x))
+<class 'int'>
+y=40.7
+print(y)
+40.7
+print(type(y))
+<class 'float'>
+x=1j
+print(x)
+1j
+print(type(x))
+<class 'complex'>
+x="hello world"
+print(x)
+hello world
+print(type(x))
+<class 'str'>
+x=["apple","banana","mango"]
+print(x)
+['apple', 'banana', 'mango']
+print(type(x))
+<class 'list'>
+x=("apple","banana","mango")
+print(x)
+('apple', 'banana', 'mango')
+print(type(x))
+<class 'tuple'>
+x=range(6)
+print(x)
+range(0, 6)
+print(type(x))
+<class 'range'>
