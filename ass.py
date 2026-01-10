e66e1c6570d4d0aff72fb0c048ce4df0df9c134e
mishri_patel <mishripatel7564@gmail.com>
2026-01-10 09:24:10 +0530

    Add files via upload

---
+++ ass.py
@@ -0,0 +1,29 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, World!")
+Hello, World!
+a=int(input("enter first num:"))
+enter first num:7
+b=int(input("enter second num:"))
+enter second num:8
+sum=a+b
+print("sum =",sum)
+sum = 15
+num=int(input("enter a num"))
+enter a num7
+if num % 2 == 0:
+    print("num is even")
+else:
+    print("num is odd")
+
+    
+num is odd
+year=int(input("enter a year:"))
+enter a year:2025
+if(year % 400 == 0) or (year % 4 == 0 and year % 100 !=0):
+    print("leap year")
+else:
+    print("not a leap year")
+
+    
+not a leap year
