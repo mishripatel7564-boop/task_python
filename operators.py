bbb742039f231006acc6d4e67460e227c8f93bac
mishri_patel <mishripatel7564@gmail.com>
2026-01-27 08:25:38 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,75 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmetic operator
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> #assigment operator
+>>> a=10
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> print(a)
+10
+>>> #unary minus
+>>> a=5
+>>> print(-a)
+-5
+>>> #relationl operators
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> #logical operators
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> #boolean
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #bitwise operators
+>>> a=5
+>>> b=3
+>>> print(a*b)
+15
+>>> print(a/b)
+1.6666666666666667
+>>> #membership operators
+>>> numbers=(1,2,3,4)
+>>> print(2 in number)
+Traceback (most recent call last):
+  File "<pyshell#35>", line 1, in <module>
+    print(2 in number)
+NameError: name 'number' is not defined
+>>> print(2 in numbers)
+True
+>>> print(5 is not numbers)
+True
+>>> #identify operators
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
